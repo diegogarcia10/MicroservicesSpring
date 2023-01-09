bc9293613dc40d7c7b7b58c8814e4365be1c7eaf
diegogarcia10 <ddjochoa.20@gmail.com>
2023-01-09 15:40:24 -0600

    commit inicial

---
+++ service-product/src/main/java/academy/digitallab/store/product/entity/Category.java
@@ -0,0 +1,17 @@
+package academy.digitallab.store.product.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tbl_categories")
+@AllArgsConstructor @NoArgsConstructor @Builder
+@Getter @Setter
+@Data
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
