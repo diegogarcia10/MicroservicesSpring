cb501d15f820330bd883123460d04492259b0d87
diegogarcia10 <ddjochoa.20@gmail.com>
2023-01-10 13:49:07 -0600

    Actualizando confiuracion 2

--- shopping-service/src/main/java/academy/digitallab/store/shopping/ShoppingServiceApplication.java
+++ shopping-service/src/main/java/academy/digitallab/store/shopping/ShoppingServiceApplication.java
@@ -2,6 +2,7 @@ package academy.digitallab.store.shopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
@@ -13,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @EnableHystrix
 @EnableHystrixDashboard
+@EnableCircuitBreaker
 public class ShoppingServiceApplication {
 
 	public static void main(String[] args) {
