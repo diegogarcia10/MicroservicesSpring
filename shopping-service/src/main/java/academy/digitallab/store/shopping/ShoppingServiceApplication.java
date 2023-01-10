69ed15a8757ba3911e2ff434caf8412a2c7eab50
diegogarcia10 <ddjochoa.20@gmail.com>
2023-01-10 14:24:26 -0600

    cambios 5

--- shopping-service/src/main/java/academy/digitallab/store/shopping/ShoppingServiceApplication.java
+++ shopping-service/src/main/java/academy/digitallab/store/shopping/ShoppingServiceApplication.java
@@ -14,7 +14,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @EnableHystrix
 @EnableHystrixDashboard
-@EnableCircuitBreaker
 public class ShoppingServiceApplication {
 
 	public static void main(String[] args) {
